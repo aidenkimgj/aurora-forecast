f6b23b0483148afb1a8f907c0cee8159b94155f1
Aiden Kim <aidengookjinkim@gmail.com>
2021-04-05 23:41:44 -0600

    Create Weather component

---
+++ src/components/weather/Weather.jsx
@@ -0,0 +1,56 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'reactstrap';
+import CurrentWeather from './CurrentWeather';
+import DailyWeather from './DailyWeather';
+import HourlyWeather from './HourlyWeather';
+
+const Weather = ({ location }) => {
+  const [weather, setWeather] = useState();
+  const OPENWEATHER_API = process.env.REACT_APP_OPENWEATHER_API_KEY;
+  console.log(location, '날씨 컴포넌트');
+
+  const getWeather = async () => {
+    const data = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lng}&exclude=minutely&units=metric&appid=${OPENWEATHER_API}`
+    );
+    console.log('시간 ===>', new Date(data.data.current.dt * 1000));
+    console.log('날씨 데이터 ====>', data);
+
+    setWeather(data.data);
+  };
+
+  console.log('state의 날씨 ==>', weather);
+
+  useEffect(() => {
+    getWeather();
+  }, [location]);
+
+  return (
+    <>
+      {weather ? (
+        <>
+          <div className="weather">
+            <Row className="weather-current">
+              <Col xs="6" className="current">
+                <CurrentWeather currWeather={weather.current} />
+              </Col>
+              <Col xs="6" className="hourly">
+                <HourlyWeather hourlyWeather={weather.hourly} />
+              </Col>
+            </Row>
+            <Row className="weather-daily">
+              <Col>
+                <DailyWeather dailyWeather={weather.daily} />
+              </Col>
+            </Row>
+          </div>
+        </>
+      ) : (
+        <h3>Weather is loading...</h3>
+      )}
+    </>
+  );
+};
+
+export default Weather;
