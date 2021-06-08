ee80caeb9f7018c7773009e47a7614352f903b65
Aiden Kim <aidengookjinkim@gmail.com>
2021-06-07 22:44:21 -0600

    Check aurora data

--- src/components/aurora/Aurora.jsx
+++ src/components/aurora/Aurora.jsx
@@ -3,12 +3,21 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 const Aurora = React.memo(location => {
-  const [data, setDate] = useState();
+  const [info, setInfo] = useState();
   const getAuroraForecast = async (latitude, longitude) => {
     const data = await axios.get(
       `https://api.auroras.live/v1/?type=ace&lat=${latitude}&long=${longitude}&data=all`
     );
-    console.log('오로라 데이터', data);
+    const threedays = await axios.get(
+      `https://api.auroras.live/v1/?type=ace&lat=${latitude}&long=${longitude}&data=threeday`
+    );
+    const probability = await axios.get(
+      `https://api.auroras.live/v1/?type=ace&lat=${latitude}&long=${longitude}&data=probability`
+    );
+    console.log('오로라 데이터 all', data);
+    console.log('오로라 데이터 threeday', threedays);
+    console.log('오로라 데이터 probability', probability);
+    setInfo(data);
   };
 
   useEffect(() => {
